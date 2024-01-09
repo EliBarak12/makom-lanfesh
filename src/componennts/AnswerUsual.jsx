1178ebfc1e47bf46e8106b8d292746e9399a5515
eli barak <barak3656@gmail.com>
2024-01-09 13:02:44 +0200

    change the text

--- src/componennts/AnswerUsual.jsx
+++ src/componennts/AnswerUsual.jsx
@@ -44,11 +44,7 @@ const AnswerUsual = () => {
                 </p>
                
 
-                <div className="w-full flex justify-center mt-2">
-                  <a href={FILL_FORM_URL} className="w-1/2 py-2 px-4 bg-blue-200 text-black rounded-lg shadow-md hover:bg-blue-400">
-                    {dataHebrew.findProCare.fillFormButton}
-                  </a>
-                </div>
+                
               </div>
             </div>
           </section>
